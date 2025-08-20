df2c932d1e15624ac1969cff2693aba4ab0c02cb
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-08-20 22:01:17 +0530

    20.08

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+console.log("sowanshi");
+console.log("58");
+console.log("new delhi");
+
+var schoolName = "KVS"
+var class_ = "6th";
+var rollNo = 26;
+
+console.log(schoolName, class_, rollNo);
+
+alert("welcome to javascript learning!");
+
+var favFood = prompt("enter your fav food");
+console.log(favFood);
+
+var response = confirm ("Do you want to learn more JavaScript?");
+console.log(response)
+
+
+var a = alert("something went wrong")
+var b = confirm("are you sure to delete these files")
+var c = prompt("enter username to proceed")
+
+console.log(a, b, c)
