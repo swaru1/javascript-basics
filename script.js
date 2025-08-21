3b5e048fec74f1b2bd6b0983dc290ddce0975e56
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-08-21 16:24:17 +0530

    ques

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
+var sideA = 4;
+var sideB = 4;
 
-
-console.log("sowanshi");
-console.log("58");
-console.log("new delhi");
-
-var schoolName = "KVS"
-var class_ = "6th";
-var rollNo = 26;
-
-console.log(schoolName, class_, rollNo);
-
-alert("welcome to javascript learning!");
-
-var favFood = prompt("enter your fav food");
-console.log(favFood);
-
-var response = confirm ("Do you want to learn more JavaScript?");
-console.log(response)
-
-
-var a = alert("something went wrong")
-var b = confirm("are you sure to delete these files")
-var c = prompt("enter username to proceed")
-
-console.log(a, b, c)
+if(sideA == sideB ) {
+    console.log("it is a square");
+}else {
+    console.log (" it is a retangle");
+}
