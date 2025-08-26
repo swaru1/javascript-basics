159a6d4e5f2c1b1889d7e79d67446f349b49472b
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-08-26 16:26:34 +0530

    25/8

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-var salary = Number(prompt("enter your salary"));
+/* var a = 10; //is var me wo number hoga jiski table print karni hai
 
-/* if(salary > 10000 && salary < 20000) {
-  salary = salary + ((salary/100) * 10); 
-  console.log(" the raised salary is " + salary)
-}else{
-  console.log("you are not elegible for a raise")
+var b = 1 //yaha humne naya var initalise kiya 1 se, is hi var pe hum loop karenge
+
+while ( b <= 10) { //jab tak b jo ki 1 hai wo 10 nhi ho jata tab tak run karo 
+
+    console.log( a + " x " + b + " = " + a*b )  // output ke liye [10] x [1] = [10]   
+
+    b++;   // var b jo ki 1 tha usko increment kr do aur loop me wapis jao
 } */
 
 
-var salary = Number(prompt("enter your salary"));
+var num = Number(prompt("enter a number")) //promt ke reply ko number me convert karo and var num me store karo
+var aa = 1;     //var aa jisme 1 hai isey hum loop karne ke liye use karenge 
+console.log("even numbers till " + num + " are..");
 
-if(salary > 10000 && salary < 20000) {
-  salary = salary - ((salary/100) * 5); 
-  console.log(" the deducted salary is " + salary)
-}else{
-  console.log("you are not elegible for a raise")
+while ( aa <= num ) {           //jab tak var aa var num ke barabar nahi hota tab tak loop run karo
+
+    if( aa % 2 == 0) {          //agar var aa/2 ka remainder = 0 i.e case for even no hai to block ka code run karo 
+        console.log(aa);
+    }
+    aa++;      // var aa jiski value 1 thi usme +1 kardo
 }
-
